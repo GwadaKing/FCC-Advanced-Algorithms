8fe346d1e519eb4cfafc188460004d4b297b1702
GwadaKing <davidmiolard@laposte.net>
2017-05-01 01:04:09 -0400

    VDEF challenge termine

--- record-collection.js
+++ record-collection.js
@@ -29,19 +29,23 @@ var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
 function updateRecords(id, prop, value) {
-  "use strict";  
-  if (value==="") {
-    delete collection[id][prop];
-  } 
-  else if (prop!=="tracks") {
+  // si tracks n'existe pas dans l'album, créer un tableau vide
+  if (prop==="tracks" && (!(collection[id]).tracks)) {
+    collection[id].tracks = [];
+  }
+  // si tracks a une valeur, la rajouter
+  if (prop === "tracks" && value !== "") {
+    collection[id].tracks.push(value);
+  }
+  // sinon si prop différent de tracks et value existe alors le rajouter
+  else if (prop !== "tracks" && value !== "") {
     collection[id][prop] = value;
-  } 
-  else {
-    collection[id][prop]=[];
-    collection[id][prop].push(value);
+  }
+  // sinon si value vide alors effacer la propriété correspondante
+  else if (value === "") {
+    delete collection[id][prop];
   }
   return collection;
 }
 // Alter values below to test your code
-updateRecords(5439, "tracks", "Take a Chance on Me");
-
+updateRecords(2468, "tracks", "Free");
